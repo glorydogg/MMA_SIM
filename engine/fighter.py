df6ecfbfeccd9eea483ff68228cb47e5aee20b69
Kanan Dimanche <kdimanche1@gmail.com>
2025-11-01 18:13:00 -0400

    Added base fighter class

---
+++ engine/fighter.py
@@ -0,0 +1,34 @@
+import random
+class Fighter:
+    def __init__(self, name, power, accuracy, stamina):
+        self.name = name
+        self.power = power
+        self.accuracy = accuracy
+        self.stamina = stamina
+        self.health = 100
+
+   
+
+    def take_damage(self, dmg):
+        self.health = max(0, self.health - dmg)
+    
+    def use_stamina(self):
+        self.stamina = max(0, self.stamina - 5)
+    
+    def recover_stamina(self):
+        self.stamina = max(0, self.stamina + 10) 
+
+    def get_stamina(self):
+        return self.stamina
+    
+    def get_health(self):
+        return self.health
+
+    def is_knocked_out(self):
+        if self.health <= 0:
+            print(f"{self.name} has been kncoked out!")
+            return True
+        return False
+
+
+
